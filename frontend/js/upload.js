1dcec062ad6d6de24e3bcac2cda58c5b27bb904f
taladameghana <taladameghana00@gmail.com>
2025-12-22 16:18:00 +0530

    Add uploadImage function for image uploads

--- frontend/js/upload.js
+++ frontend/js/upload.js
@@ -1,1 +1,10 @@
+export function uploadImage(endpoint, file) {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  return fetch(endpoint, {
+    method: "POST",
+    body: formData
+  }).then(res => res.json());
+}
 

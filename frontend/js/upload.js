2d2863df9c2fd11b7c6cb7d7809e4c3eba72ed50
taladameghana <taladameghana00@gmail.com>
2025-12-22 16:33:26 +0530

    Update upload.js

--- frontend/js/upload.js
+++ frontend/js/upload.js
@@ -7,4 +7,14 @@ export function uploadImage(endpoint, file) {
     body: formData
   }).then(res => res.json());
 }
+import { uploadImage } from "./upload.js";
+
+document.getElementById("imageInput").addEventListener("change", async (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        const response = await uploadImage("http://127.0.0.1:8000/process/upload/", file);
+        console.log("Upload Response:", response);
+    }
+});
+
 

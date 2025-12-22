db1d7c998b46d606f8a5b41713d21bd3737cf836
taladameghana <taladameghana00@gmail.com>
2025-12-22 16:28:31 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,16 @@ from fastapi import FastAPI
 from backend.database import Base, engine
 from backend.routers import attendance_routes, stress_routes, process_image
 
-app = FastAPI(title="Smart Attendance & Wellbeing System")
-
+# Create tables
 Base.metadata.create_all(bind=engine)
 
-app.include_router(attendance_routes.router, prefix="/attendance")
-app.include_router(stress_routes.router, prefix="/stress")
-app.include_router(process_image.router, prefix="/process")
+app = FastAPI(title="Smart Student Presence & Well-Being Monitoring API")
+
+# Routers
+app.include_router(attendance_routes.router)
+app.include_router(stress_routes.router)
+app.include_router(process_image.router)
 
 @app.get("/")
 def root():
-    return {"message": "Smart Attendance & Wellbeing API Running"}
-
+    return {"message": "Smart Attendance & Wellbeing API is running"}

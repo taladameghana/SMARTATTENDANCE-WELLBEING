3cd55a047e59f6ca0dd4389fe81b943d4c735a13
taladameghana <taladameghana00@gmail.com>
2025-12-22 15:54:41 +0530

    Add face detection functionality using MediaPipe

--- ai_modules/facerecognition/detector.py
+++ ai_modules/facerecognition/detector.py
@@ -1,1 +1,9 @@
+import mediapipe as mp
+
+mp_face = mp.solutions.face_detection
+
+def detect_faces(image_np):
+    with mp_face.FaceDetection(model_selection=0, min_detection_confidence=0.5) as detector:
+        results = detector.process(image_np)
+        return results.detections
 

76d1a9749b509be63070a73b498f7fab0c0fe92a
taladameghana <taladameghana00@gmail.com>
2025-12-22 15:56:33 +0530

    Add function to determine stress level based on emotion

--- ai_modules/facerecognition/emotionstress/stresslogic.py
+++ ai_modules/facerecognition/emotionstress/stresslogic.py
@@ -1,1 +1,8 @@
+def get_stress_level(emotion):
+    if emotion in ["Sad", "Stressed"]:
+        return "High"
+    elif emotion == "Neutral":
+        return "Medium"
+    else:
+        return "Low"
 
